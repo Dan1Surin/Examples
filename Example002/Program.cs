83387fdc6b7c8dd25b200f395992bb993daeca57
Даниил Сурин <dan1surin@gmail.com>
2022-12-14 14:43:54 +0300

    use readline and writeline

---
+++ Example002/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Привет, как тебя зовут?");
+string username = Console.ReadLine();
+Console.WriteLine("Привет");
+Console.WriteLine(username); 
